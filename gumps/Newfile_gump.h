ce48835efd2679c36c68c9878fbdabe13e55fa92
Alexis Ryan <the.wench@wenchy.net>
2025-10-17 06:47:20 +1100

    Major update to Newfile_gump. It now uses a Scrollable_widget for scrolling instead of handling all scrolling logic itself.  Replaced negative slot numbers with an enum and improved bounds checking in some places. It can now be forced into a mode where it looks and behaves exactly like the old classic File_gump by setting <config/disk/use_old_style_save_load> to yes, Removed now unneeded File_gump class just leaving a stub that opens Newfile_gump in old style mode. Gamewin no longer has the save_names array that was only needed by File_gump  Newfile_gump no longer needs any shapes in exult.flx and all the shapes havebeen removed

--- gumps/Newfile_gump.h
+++ gumps/Newfile_gump.h
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2000-2024 The Exult Team
+Copyright (C) 2000-2025 The Exult Team
 
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "Modal_gump.h"
 #include "SaveInfo.h"
+#include "Scrollable_widget.h"
 
 #include <array>
 #include <memory>
@@ -40,126 +41,150 @@ protected:
 		id_save,
 		id_delete,
 		id_close,
-		id_page_up,
-		id_line_up,
-		id_line_down,
-		id_page_down,
-		id_count
+		id_music,
+		id_speech,
+		id_effects,
+		id_count,
 	};
 
 	std::array<std::unique_ptr<Gump_button>, id_count> buttons;
+	std::array<std::unique_ptr<Gump_button>, id_count> disabled_buttons;
 
+	// Enable or disable a button
+	void SetButtonEnabled(button_ids id, bool newenabled);
 	// Button Coords
-	const short btn_rows[5] = {186, 2, 15, 156, 169};
-	const short btn_cols[5] = {2, 46, 88, 150, 209};
+	constexpr static std::array<short, 1> btn_rows = {
+			186,
+	};
+	constexpr static std::array<short, 4> btn_cols = {2, 46, 88, 150};
+	constexpr static std::array<short, 2> old_btn_rows{132, 155};
+	constexpr static std::array<short, 3> old_btn_cols{95, 164, 233};
 
 	// Text field info
-	const short fieldx     = 2;      // Start Y of each field
-	const short fieldy     = 2;      // Start X of first
-	const short fieldw     = 207;    // Width of each field
-	const short fieldh     = 12;     // Height of each field
-	const short fieldgap   = 1;      // Gap between fields
-	const short fieldcount = 14;     // Number of fields
-	const short textx      = 12;     // X Offset in field
-	const short texty      = 2;      // Y Offset in field
-	const short textw      = 190;    // Maximum allowable width of text (pixels)
-	const short iconx      = 2;      // X Offset in field
-	const short icony      = 2;      // Y Offset in field
+	constexpr static short fieldx   = 2;      // Start Y of each field
+	constexpr static short fieldy   = 2;      // Start X of first
+	constexpr static short fieldw   = 208;    // Width of each field
+	constexpr static short fieldh   = 12;     // Height of each field
+	constexpr static short fieldgap = 1;      // Gap between fields
+	constexpr static short textx    = 12;     // X Offset in field
+	constexpr static short texty    = 2;      // Y Offset in field
+	constexpr static short textw
+			= fieldw - 17;    // Maximum allowable width of text (pixels)
+	constexpr static short iconx = 2;    // X Offset in field
+	constexpr static short icony = 2;    // Y Offset in field
 
-	// Scrollbar and Slider Info
-	const short scrollx = 212;    // X Offset
-	const short scrolly = 28;     // Y Offset
-	const short scrollh = 129;    // Height of Scroll Bar
-	const short sliderw = 7;      // Width of Slider
-	const short sliderh = 7;      // Height of Slider
+	constexpr static short scrollx = 2, scrolly = 2;
 
-	const short infox = 224;
-	const short infoy = 67;
-	const short infow = 92;
-	const short infoh = 79;
+	// Info box info
+	constexpr static short infox = 224;
+	constexpr static short infoy = 65;
+	constexpr static short infow = 92;
+	constexpr static short infoh = 79;
 
-	unsigned char restored = 0;    // Set to 1 if we restored a game.
+	bool restored = false;    // Set true if we restored a game.
 
 	enum SaveSlots {
+		InvalidSlot   = -5,
+		NoSlot        = -4,
 		EmptySlot     = -3,
-		GamedatSlot   = -2,
-		QuicksaveSlot = -1,
+		QuicksaveSlot = -2,
+		GamedatSlot   = -1,
+		SavegameSlots = 0
 	};
 
 	std::unique_ptr<Image_buffer> back;
 
-	const std::vector<SaveInfo> *games;    // The list of savegames
-	std::vector<SaveInfo> old_games;    // The list of savegames
+	const std::vector<SaveInfo>* games
+			= nullptr;                  // The list of savegames being shown
+	std::vector<SaveInfo> old_games;    // Backing storage for the  list of
+										// savegames in old_style_mode
 
-	std::unique_ptr<Shape_file> cur_shot;    // Screenshot for current game
-	SaveGame_Details cur_details;    // Details of current game
+	std::unique_ptr<Shape_file> cur_shot;       // Screenshot for current game
+	SaveGame_Details            cur_details;    // Details of current game
 	std::vector<SaveGame_Party> cur_party;      // Party of current game
 
 	// Gamedat is being used as a 'quicksave'
-	int last_selected = -4;    // keeping track of the selected line for iOS
+	int last_selected
+			= InvalidSlot;    // keeping track of the selected line for iOS
 	std::unique_ptr<Shape_file> gd_shot;       // Screenshot in Gamedat
 	SaveGame_Details            gd_details;    // Details in Gamedat
 	std::vector<SaveGame_Party> gd_party;      // Party in Gamedat
 
-	const Shape_file*       screenshot  = nullptr;    // The picture to be drawn
-	const SaveGame_Details* details     = nullptr;    // The game details to show
-	const  std::vector<SaveGame_Party> *party       = nullptr;    // The party to show
-	bool              is_readable = false;      // Is the save game readable
-	const char* filename = nullptr;    // Filename of the savegame, if exists
+	const Shape_file*       screenshot = nullptr;    // The picture to be drawn
+	const SaveGame_Details* details    = nullptr;    // The game details to show
+	const std::vector<SaveGame_Party>* party = nullptr;    // The party to show
+	bool        is_readable = false;      // Is the save game readable
+	const char* filename    = nullptr;    // Filename of the savegame, if exists
 
-	int  list_position = -2;    // The position in the savegame list (top game)
-	int  selected = -3;    // The savegame that has been selected (num in list)
-	int  cursor   = 0;     // The position of the cursor
-	int  slide_start = -1;                  // Pixel (v) where a slide started
+	int fieldcount;    // Number of slots being shown by the gump: 14 in normal
+					   // or restore mode, 10 in olde_style_mode
+	int selected_slot
+			= NoSlot;    // The savegame slot that has been selected
+	int  cursor = -1;    // The position of the cursor ( -1 is no cursor)
 	char newname[MAX_SAVEGAME_NAME_LEN];    // The new name for the game
 
 	// Run in reduced functionality restore mode. Can only load and delete
 	// games. Loading games only Restores Gamedat
+	// Can not be combined with old_style_mode. Restore mode takes Presedence
 	bool restore_mode;
 
-	int BackspacePressed();
-	int DeletePressed();
-	int MoveCursor(int count);
-	int AddCharacter(char c);
+	// Old style mode replicates the functionality of the old File_gump and
+	// how it works in the original games There are 10 save slots only. Each
+	// slot can be edited. There is no scrolling or deleting. There  are
+	// Quit, Sound, Speech and Music Buttons.
+	// Cannot be combined with restore_mode
+
+	bool                  old_style_mode;
+	std::shared_ptr<Font> tinyfont;
+
+	int  BackspacePressed();
+	int  DeletePressed();
+	int  MoveCursor(int count);
+	int  AddCharacter(char c);
+	void SelectSlot(int slot);    // Select a given slot
 
 	void LoadSaveGameDetails();    // Loads (and sorts) all the savegame details
 	void FreeSaveGameDetails();    // Frees all the savegame details
 
-
 public:
 	// Construct a Newfile_gump. Optionally using reduced functionality restore
-	// mode
-	Newfile_gump(bool restore_mode = false);
+	// mode or old style mode. Can not use both modes at once. restore_mode
+	// takes Presedence
+	Newfile_gump(bool restore_mode = false, bool old_style_mode = false);
 	~Newfile_gump() override;
 
+	// Handle one of the toggles.
+	void toggle_audio_option(Gump_widget* btn, int state);
 	void load();           // 'Load' was clicked.
 	void save();           // 'Save' was clicked.
 	void delete_file();    // 'Delete' was clicked.
 
-	void scroll_line(int dir);    // Scroll Line Button Pressed
-	void scroll_page(int dir);    // Scroll Page Button Pressed.
 
-	void line_up() {
-		scroll_line(-1);
+	// Get the last slot
+	sint64 LastSlot() const {
+		return SavegameSlots + (games ? games->size() - 1 : 0);
 	}
 
-	void line_down() {
-		scroll_line(1);
+	// Get the first slot
+	sint64 FirstSlot() const {
+		if (restore_mode) {
+			return GamedatSlot;
+		}
+		if (!old_style_mode) {
+			return EmptySlot;
+		} else {
+			return SavegameSlots;
+		}
 	}
 
-	void page_up() {
-		scroll_page(-1);
+	// Get the total nuber of slots
+	sint64 NumSlots() const {
+		return LastSlot() + 1 - FirstSlot();
 	}
 
-	void page_down() {
-		scroll_page(1);
-	}
-
-	int restored_game() {    // 1 if user restored.
+	bool restored_game() {    // true if user restored.
 		return restored;
 	}
-
-	void PaintSaveField(int line, Image_buffer8* ibuf);
 
 	// Paint it and its contents.
 	void paint() override;
@@ -167,6 +192,8 @@ public:
 	void close() override {
 		done = true;
 	}
+
+	void quit();
 
 	// Handle events:
 	bool text_input(const char* text) override;    // Character typed.
@@ -178,6 +205,29 @@ public:
 
 	bool mousewheel_up(int mx, int my) override;
 	bool mousewheel_down(int mx, int my) override;
+
+private:
+	class Slot_widget : public Gump_widget {
+		Newfile_gump* nfg;
+
+	public:
+		Slot_widget(Newfile_gump* nfg)
+				: Gump_widget(nfg, 0, 0, 0, -1, SF_OTHER), nfg(nfg) {}
+
+		bool mouse_down(int mx, int my, MouseButton button) override;
+
+		TileRect get_rect() const override {
+			TileRect ret(
+					0, 0, fieldw,
+					(fieldh + fieldgap) * nfg->NumSlots() - fieldgap);
+			local_to_screen(ret.x, ret.y);
+			return ret;
+		}
+
+		void paint() override;
+	};
+
+	std::unique_ptr<Scrollable_widget> scroll;
 };
 
 #endif    // NEWFILE_GUMP_H
