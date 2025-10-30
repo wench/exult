722b409fc82bf51e06a47731f212db86418df6fb
Alexis Ryan <the.wench@wenchy.net>
2025-10-31 01:14:32 +1100

    Add a Settings class to automate and simplify using config values. Ultimately the goal of this class is to be used for global settings instead of just putting them in  Game_window. Instead of using getter and setter methods, settings in the Settings class are templated property opjects that automatically convert to their underlying type and can be set by just assigning to them. Settings class doesn't contain any useful settings properties as of this commit Just some settings for future save game changes, but they give a general idea how the class would be used

---
+++ conf/Settings.cc
@@ -0,0 +1,136 @@
+#include "Settings.h"
+
+#include "Configuration.h"
+
+const std::vector<std::pair<std::string, bool>> Settings::boolyesnoany = {
+		{  "yes",  true},
+        {   "no", false},
+        { "true",  true},
+        {"false", false},
+		{    "1",  true},
+        {    "0", false},
+        {   "on",  true},
+        {  "off", false}
+};
+const std::vector<std::pair<std::string, bool>> Settings::booltruefalseany = {
+		{ "true",  true},
+        {"false", false},
+        {  "yes",  true},
+        {   "no", false},
+		{    "1",  true},
+        {    "0", false},
+        {   "on",  true},
+        {  "off", false}
+};
+const std::vector<std::pair<std::string, bool>> Settings::boolonoffany = {
+		{   "on",  true},
+        {  "off", false},
+        {  "yes",  true},
+        {   "no", false},
+		{ "true",  true},
+        {"false", false},
+        {    "1",  true},
+        {    "0", false},
+};
+Settings Settings::instance;
+
+bool Settings::property::config_write(const char* s, bool writeback) {
+	if (!instance.configuration) {
+		return false;
+	}
+	instance.configuration->set(config_key, s, writeback);
+	return true;
+}
+
+bool Settings::property::config_read(
+		std::string& s, const std::string& defaultvalue) {
+	if (!instance.configuration) {
+		s = defaultvalue;
+		return false;
+	}
+	return instance.configuration->value(config_key, s, defaultvalue);
+}
+
+bool Settings::property::config_write(const std::string& s, bool writeback) {
+	if (!instance.configuration) {
+		return false;
+	}
+	instance.configuration->set(config_key, s, writeback);
+	return true;
+}
+
+bool Settings::property::config_read(int& i, int defaultvalue) {
+	if (!instance.configuration) {
+		i = defaultvalue;
+		return false;
+	}
+	return instance.configuration->value(config_key, i, defaultvalue);
+}
+
+bool Settings::property::config_write(int i, bool writeback) {
+	if (!instance.configuration) {
+		return false;
+	}
+		instance.configuration->set(config_key, i, writeback);
+	return true;
+	}
+
+void Settings::save_all(bool write_out) {
+	for (auto* set : property_sets) {
+		set->save_all(false);
+	}
+	if (write_out && configuration) {
+		instance.configuration->write_back();
+	}
+}
+
+inline void Settings::save_dirty(bool write_out) {
+	for (auto* set : property_sets) {
+		set->save_dirty(false);
+	}
+	if (write_out && configuration) {
+		configuration->write_back();
+	}
+}
+
+void Settings::load_all(Configuration* config) {
+	this->configuration = config;
+	if (configuration) {
+		for (auto* set : property_sets) {
+			set->load_all();
+		}
+	}
+		
+	}
+
+void Settings::PropertySet::save_all(bool write_out) {
+	for (auto* prop : *this) {
+		prop->save(false);
+	}
+	if (write_out) {
+		instance.configuration->write_back();
+	}
+}
+
+void Settings::PropertySet::load_all() {
+	bool write_out = false;
+	for (auto* prop : *this) {
+		if (!prop->get_synced_to_config()) {
+			write_out |= prop->load();
+		}
+	}
+	if (write_out) {
+		instance.configuration->write_back();
+	}
+}
+
+void Settings::PropertySet::save_dirty(bool write_out) {
+	for (auto* prop : *this) {
+		if (prop->dirty()) {
+			prop->save(false);
+		}
+	}
+	if (write_out) {
+		instance.configuration->write_back();
+	}
+}
